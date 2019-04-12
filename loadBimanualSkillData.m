6a2958902ad8d86fc75ad4d5e40b9d86778658a3
Chris <Shimohara4@gmail.com>
2019-04-12 19:43:08 -0400

    Removed unnecessary code

--- loadBimanualSkillData.m
+++ loadBimanualSkillData.m
@@ -3,36 +3,12 @@
 % This example loads a single 'chunk' (block of 5 trials)
 
 clear all
-% rot_vs_mir
 names{1} = {'subj17','subj18','subj21','subj22','subj24','subj25','subj28','subj31','subj32','subj33'};
 names{2} = {'subj14','subj15','subj16','subj19','subj23','subj26','subj27','subj29','subj30','subj34'};
 groups = {'rot','mir'};
 path = 'Data/rot_vs_mir/';
 blocks = {'no_rot1','rot1','rot2','rot3'};
 START = repmat([0.8 0.3],[10 1]);
-
-% 10P2P
-% names{1} = {'subj37','subj38','subj39','subj40','subj41'};
-% names{2} = {'subj42','subj43','subj44'};
-% groups = {'rot','mir'};
-% path = 'Data/10P2P/';
-% blocks = {'no_rot1','rot1','rot2','rot3'};
-% START = repmat([0.8 0.3],[10 1]);
-
-% no_P2P_15deg
-% names{1} = {'subj1','subj2','subj3','subj4','subj5','subj6','subj7','subj8','subj9','subj10'};
-% groups = {'rot'};
-% path = 'Data/no_P2P_15deg/';
-% blocks = {'baseline','early','late','after'};
-% START = repmat([0.6 0.25],[9 1]);
-% START = [START; 0.6 0.3];
-
-% no_P2P_15deg experiment 2
-% names{1} = {'subj11','subj12','subj13','subj14','subj15','subj17','subj18','subj19','subj20','subj21'};
-% groups = {'rot'};
-% path = 'Data/no_P2P_15deg/';
-% blocks = {'baseline','rot1','rot2','rot3','after'};
-% START = repmat([0.8 0.3],[10 1]);
 
 for i = 1:length(names)
     subjnames = names{i};
--- plot_direction.m
+++ plot_direction.m
@@ -5,12 +5,7 @@ names = {'Rotation','Mirror Reversal'};
 
 col = lines;
 col = col(1:7,:);
-% groups = {'rot'};
-% names = {'Rotation'};
 Nsubj = length(d.rot);
-    
-% a = figure;
-% c = figure;
 Ntrials = length(d.(groups{1}){1}.Cr);
 Ntrials2 = Ntrials/length(blocks);
 
@@ -33,32 +28,32 @@ for i = 1:length(groups)
     edges = -50:10:130;
     
     % plot unbinned direction histograms
-%     figure(a)
-%     subplot(4,2,(i-1)+1)
-%     histogram(dir(1:150),edges)
-%     axis([-25 135 0 75])
-%     xticks(-45:45:135)
-%     title(names{i})
-% 
-%     subplot(4,2,(i-1)+3)
-%     histogram(dir(151:300),edges)
-%     axis([-25 135 0 75])
-%     xticks(-45:45:135)
-%     title('Early Learning')
-%     ylabel('Counts')
-% 
-%     subplot(4,2,(i-1)+5)
-%     histogram(dir(301:450),edges)
-%     axis([-25 135 0 75])
-%     xticks(-45:45:135)
-%     title('Mid Learning')
-% 
-%     subplot(4,2,(i-1)+7)
-%     histogram(dir(451:600),edges)
-%     axis([-25 135 0 75])
-%     xticks(-45:45:135)
-%     title('Late Learning')
-%     xlabel('Hand Reach Angle')
+    figure(1)
+    subplot(4,2,(i-1)+1)
+    histogram(dir(1:150),edges)
+    axis([-60 90 0 75])
+    xticks(-45:45:135)
+    title(names{i})
+
+    subplot(4,2,(i-1)+3)
+    histogram(dir(151:300),edges)
+    axis([-60 90 0 75])
+    xticks(-45:45:135)
+    title('Early Learning')
+    ylabel('Counts')
+
+    subplot(4,2,(i-1)+5)
+    histogram(dir(301:450),edges)
+    axis([-60 90 0 75])
+    xticks(-45:45:135)
+    title('Mid Learning')
+
+    subplot(4,2,(i-1)+7)
+    histogram(dir(451:600),edges)
+    axis([-60 90 0 75])
+    xticks(-45:45:135)
+    title('Late Learning')
+    xlabel('Hand Reach Angle')
 
     % plot reach error binned by 4 trials
     figure
@@ -102,93 +97,5 @@ for i = 1:length(groups)
     yticks(-45:45:45)
     ylabel(['Reach Direction Error (',char(176),')'])
     xlabel('Trial Number')
-
-%     no_P2P_15deg
-%     if i == 1
-%         x = shadedErrorBar(1:30,dir(1:30),se(1:30));
-%         editErrorBar(x,[1 0 0],1);
-%         hold on
-%         plot([1 150],[0 0],'--k','LineWidth',1)
-%         x = shadedErrorBar(31:60,dir(31:60),se(31:60));
-%         editErrorBar(x,[1 0 0],1);
-%         x = shadedErrorBar(61:90,dir(61:90),se(61:90));
-%         editErrorBar(x,[1 0 0],1);
-%         x = shadedErrorBar(91:120,dir(91:120),se(91:120));
-%         editErrorBar(x,[1 0 0],1);
-%         x = shadedErrorBar(121:150,dir(121:150),se(121:150));
-%         editErrorBar(x,[1 0 0],1);
-%         rectangle('Position',[1 -200 Ntrials2 400],'FaceColor',[col(1,:) 0.1],'EdgeColor','none')
-%         rectangle('Position',[Ntrials2+1 -200 Ntrials2 400],'FaceColor',[col(2,:) 0.1],'EdgeColor','none')
-%         rectangle('Position',[3*Ntrials2+1 -200 Ntrials2 400],'FaceColor',[col(3,:) 0.1],'EdgeColor','none')
-%         rectangle('Position',[4*Ntrials2+1 -200 Ntrials2 400],'FaceColor',[col(4,:) 0.1],'EdgeColor','none')
-%     end
-%     axis([1 150 -30 30])
-%     xticks(1:30:150)
-%     ylabel(['Reach Error (',char(176),')'])
-%     xlabel('Trials')
-
-
-    % plot direction angular error/learning rate
-%     figure(c)
-% %     subplot(2,1,i)
-%     if i == 1
-%         x = shadedErrorBar(1:600,dir,se);
-%         editErrorBar(x, [1 0 0],1);
-%         hold on
-% %         x = shadedErrorBar(151:300,dir(151:300),se(151:300));
-% %         editErrorBar(x, col(2,:), 1);
-% %         x = shadedErrorBar(301:450,dir(301:450),se(301:450));
-% %         editErrorBar(x, [0 0 0], 1);
-% %         x = shadedErrorBar(451:600,dir(451:600),se(451:600));
-% %         editErrorBar(x, col(3,:), 1);
-%         plot([-1 Ntrials+1],[0 0 ],'--k','LineWidth',2)
-%         rectangle('Position',[1 -200 Ntrials2 400],'FaceColor',[col(1,:) 0.1],'EdgeColor','none')
-%         rectangle('Position',[Ntrials2+1 -200 Ntrials2 400],'FaceColor',[col(2,:) 0.1],'EdgeColor','none')
-% %         rectangle('Position',[2*Ntrials2+1 -200 Ntrials2 400],'FaceColor',[col(1,:) 0.1],'EdgeColor','none')
-%         rectangle('Position',[3*Ntrials2+1 -200 Ntrials2 400],'FaceColor',[col(3,:) 0.1],'EdgeColor','none')
-%     else
-%         x = shadedErrorBar(1:600,dir,se);
-%         editErrorBar(x,[0 0 1],1);
-%     end
-%     xticks(1:150:600)
-%     yticks(-45:45:135)
-%     axis([1 Ntrials -90 90])
-%     ylabel(['Reach Error (',char(176),')'])
-%     xlabel('Trials')
-%     title(names{i})
-    
-%     figure('Name',names{i})
-%     for j = 1:600
-%         switch j
-%             case num2cell(1:150)
-%                 subplot(2,2,1)
-%                 plot(d.(groups{i}){1}.Cr{j}(:,1),d.(groups{i}){1}.Cr{j}(:,2))
-%                 axis([-0.15 0.15 -0.05 0.25])
-%                 hold on
-%                 pbaspect([1 1 1])
-%                 title('Baseline')
-%             case num2cell(151:300)
-%                 subplot(2,2,2)
-%                 plot(d.(groups{i}){1}.Cr{j}(:,1),d.(groups{i}){1}.Cr{j}(:,2))
-%                 axis([-0.15 0.15 -0.05 0.25])
-%                 hold on
-%                 pbaspect([1 1 1])
-%                 title('Early')
-%             case num2cell(301:450)
-%                 subplot(2,2,3)
-%                 plot(d.(groups{i}){1}.Cr{j}(:,1),d.(groups{i}){1}.Cr{j}(:,2))
-%                 axis([-0.15 0.15 -0.05 0.25])
-%                 hold on
-%                 pbaspect([1 1 1])
-%                 title('Mid')
-%             case num2cell(451:600)
-%                 subplot(2,2,4)
-%                 plot(d.(groups{i}){1}.Cr{j}(:,1),d.(groups{i}){1}.Cr{j}(:,2))
-%                 axis([-0.15 0.15 -0.05 0.25])
-%                 hold on
-%                 pbaspect([1 1 1])
-%                 title('Late')
-%         end
-%     end
 end
 
